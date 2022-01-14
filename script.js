96ca79bd752bb4bc52b84eb3bbb35e8c06012c41
Angie <agladinop@gmail.com>
2022-01-14 14:13:09 -0500

    Navegación responsiva

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+const body = document.querySelector(".body");
+const nav = document.querySelector(".nav");
+const menu = document.querySelector(".nav__menu");
+const menuButton = document.querySelector(".menu__icon--hamburger");
+const closeButton = document.querySelector(".menu__icon--close");
+
+
+
+/* Show menu links */
+function showMenu() {
+    menu.classList.add("nav__menu--active");
+    menuButton.classList.add("hide");
+    closeButton.classList.add("show");
+    body.classList.add("disabledScroll");
+}
+
+/* Hide menu links */
+function hideMenu() {
+    menu.classList.remove("nav__menu--active");
+    menuButton.classList.remove("hide");
+    closeButton.classList.remove("show");
+    body.classList.remove("disabledScroll");
+}
+
+
+
+window.onscroll = function() {
+    this.scrollY > 20 ? nav.classList.add("sticky") : nav.classList.remove("sticky");
+    showCards();
+}
+
+
+
 const arrayImg = document.querySelectorAll('.galery__img') // Get all elements from DOM with this class
 
 arrayImg.forEach(function(img) {
